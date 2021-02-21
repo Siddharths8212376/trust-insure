4f6699fb626c423035b80e5c5c66c336ef182723
Siddharths8212376 <siddharth8212376@gmail.com>
2021-02-21 23:31:56 +0530

    added costumer onboarding

--- frontend/src/components/InsuranceStatus.js
+++ frontend/src/components/InsuranceStatus.js
@@ -48,6 +48,56 @@ const ConfirmDetails = ({ insurance }) => {
         <button className="btn btn-primary" onClick={() => {ConfirmAllDetails(insurance)}}>Confirm Details</button>
     )
 }
+const ConfirmPolicyFinal = ({insurance}) => {
+    console.log(insurance, 'inside confirm policy final')
+    const [confirmPolicy, setConfirmPolicy] = useState('')
+    const handleConfirmPolicy = (e) => {
+        e.preventDefault()
+        setConfirmPolicy(e.target.value)
+    }
+    const handleSubmitConfirmPolicy = async (e) => {
+        e.preventDefault()
+        const cp = confirmPolicy==='Yes'?true:false
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+        web3.eth.getAccounts().then(console.log);
+        if (web3) {
+            const accounts = await web3.eth.getAccounts()
+            const networkId = await web3.eth.net.getId()
+            const networkData = BlockSecureDeployer.networks[networkId]
+            if (networkData) {
+                const deployedContract = new web3.eth.Contract(BlockSecureDeployer.abi, networkData.address)
+                console.log(deployedContract, 'to change state', accounts[0], insurance.ID, 'also')
+                
+                // confirm details to be called.
+                if (cp===true) {
+                await deployedContract.methods.ConfirmPolicy(Number(insurance.ID)-1).send({ from: accounts[0] })
+                // update db
+                insurance.state = 7;
+                // fetch the current insurance from the db, and update it
+                await axios.put(`http://localhost:3001/api/insurances/${insurance.ID}`, insurance)
+                } else {
+                   await deployedContract.methods.Reject(Number(insurance.ID)-1).send({ from: accounts[0] }) 
+                   insurance.state = -1;
+                   await axios.put(`http://localhost:3001/api/insurances/${insurance.ID}`, insurance)
+                }
+            }
+        }
+    }
+    return (
+        <div>
+            <form>
+
+            <div className="form-group">
+                <label for="customerVer"><bold>Confirm Policy?</bold></label>
+                <input type="text" className="form-control" id="InputCustomerVer" value={confirmPolicy} placeholder="Yes/No" onChange={handleConfirmPolicy}/>
+            </div>
+
+            <button type="submit" class="btn btn-primary" onClick={handleSubmitConfirmPolicy}>Submit</button>
+            </form> 
+        </div>
+    )
+}
+
 const RequestBankUnderwritingEvent = async (insurance) => {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
       web3.eth.getAccounts().then(console.log);
@@ -68,12 +118,38 @@ const RequestBankUnderwritingEvent = async (insurance) => {
         }
     }
 }
+const RequestMedicalUnderwritingEvent = async (insurance) => {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+      web3.eth.getAccounts().then(console.log);
+      if (web3) {
+        const accounts = await web3.eth.getAccounts()
+        const networkId = await web3.eth.net.getId()
+        const networkData = BlockSecureDeployer.networks[networkId]
+        if (networkData) {
+        const deployedContract = new web3.eth.Contract(BlockSecureDeployer.abi, networkData.address)
+        console.log(deployedContract, 'to change state', accounts[0], insurance.ID, 'also')
+        
+        // confirm details to be called.
+        await deployedContract.methods.SendToBankUnderwriter(Number(insurance.ID)-1).send({ from: accounts[0] })
+        // update db
+        insurance.state = 4;
+        // fetch the current insurance from the db, and update it
+        await axios.put(`http://localhost:3001/api/insurances/${insurance.ID}`, insurance)
+        }
+    }
+}
 const RequestBankUnderwriting = ({ insurance }) => {
     console.log(insurance, 'inside req buw')
     return (
         <button className="btn btn-primary" onClick={() => {RequestBankUnderwritingEvent(insurance)}}>Request Bank Underwriting</button>
     )
 }
+const RequestMedicalUnderwriting = ({ insurance }) => {
+    return (
+        <button className="btn btn-primary" onClick={() => {RequestMedicalUnderwritingEvent(insurance)}}>Request Medical Underwriting</button>
+    )
+}
+
 const UpdateFinancialHealth = ({ insurance }) => {
     const [customerVerificationDone, setCustomerVerificationDone] = useState('')
     const [active, setActive] = useState('')
@@ -146,36 +222,238 @@ const UpdateFinancialHealth = ({ insurance }) => {
                 <label for="buwres"><bold>Bank UW Result</bold></label>
                 <input type="text" className="form-control" id="InputBUWRes" value={bankUWResult} placeholder="Enter bank UW result" onChange={handleBankUWResult}/>
             </div>
+    <button type="submit" class="btn btn-primary" onClick={handleSubmitBUWDone}>Submit</button>
+            </form>
+        </div>
+    )
+}
+const UpdateMedicalHealth = ({ insurance }) => {
+    const [physicalVerificationDone, setPhysicalVerificationDone] = useState('')
+    const [activeStatus, setActiveStatus] = useState('')
+    const [healthScore, setHealthScore] = useState('')
+    const [medUWResult, setMedUWResult] = useState('')
+    const handlePhysicalVerificationDone = (e) => {
+        e.preventDefault()
+        setPhysicalVerificationDone(e.target.value)
+    }
+    const handleActiveStatus = (e) => {
+        e.preventDefault()
+        setActiveStatus(e.target.value) 
+    }
+    const handleHealthScore = (e) => {
+        e.preventDefault()
+        setHealthScore(e.target.value)
+    }
+    const handleMedUWResult = (e) => {
+         e.preventDefault()
+        setMedUWResult(e.target.value)
+    }
+    const handleSubmitMUWDone = async (e) => {
+        e.preventDefault()
+        // console.log(customerVerificationDone, active, financialHealthPoints, bankUWResult)
+        const pv = physicalVerificationDone==='Yes'?true:false
+        const act = activeStatus==='Yes'?true:false
+        const mhp = Number(healthScore)
+        const muwr = medUWResult==='Yes'?true:false
+        console.log(pv, act, mhp, muwr)
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+        web3.eth.getAccounts().then(console.log);
+        if (web3) {
+            const accounts = await web3.eth.getAccounts()
+            const networkId = await web3.eth.net.getId()
+            const networkData = BlockSecureDeployer.networks[networkId]
+            if (networkData) {
+                const deployedContract = new web3.eth.Contract(BlockSecureDeployer.abi, networkData.address)
+                console.log(deployedContract, 'to change state', accounts[0], insurance.ID, 'also')
+                
+                // confirm details to be called.
+                await deployedContract.methods.UpdateMedicalHealth(Number(insurance.ID)-1, act, mhp, pv, muwr).send({ from: accounts[0] })
+                // update db
+                insurance.state = 5;
+                insurance.healthScore=mhp;
+                insurance.physicalVerification=pv;
+                insurance.activeStatus=act;
+                insurance.medUWResult=muwr;
+                // fetch the current insurance from the db, and update it
+                await axios.put(`http://localhost:3001/api/insurances/${insurance.ID}`, insurance)
+            }
+        }
+    }
+    return (
+        <div>
+            <form>
+
+            <div className="form-group">
+                <label for="physicalVer"><bold>Physical Verification Done (Yes/No)</bold></label>
+                <input type="text" className="form-control" id="InputPhysicalVer" value={physicalVerificationDone} placeholder="Yes/No" onChange={handlePhysicalVerificationDone}/>
+            </div>
+            <div className="form-group">
+                <label for="active"><bold>Active Status (Yes/No)</bold></label>
+                <input type="text" className="form-control" id="InputActive" value={activeStatus} placeholder="Application active or not" onChange={handleActiveStatus}/>
+            </div>
+            <div className="form-group">
+                <label for="mhp"><bold>Medical Health Score (out of 100)</bold></label>
+                <input type="text" className="form-control" id="InputMHP" value={healthScore} placeholder="Enter medical health score" onChange={handleHealthScore}/>
+            </div>
+            <div className="form-group">
+                <label for="muwres"><bold>Medical UW Result</bold></label>
+                <input type="text" className="form-control" id="InputMUWRes" value={medUWResult} placeholder="Enter medical UW result" onChange={handleMedUWResult}/>
+            </div>
            
           
             
             
-    <button type="submit" class="btn btn-primary" onClick={handleSubmitBUWDone}>Submit</button>
+    <button type="submit" class="btn btn-primary" onClick={handleSubmitMUWDone}>Submit</button>
+            </form>
+        </div>
+    )
+}
+const RecalculatePolicy = ({ insurance }) => {
+    const [premiumReceived, setPremiumReceived] = useState('')
+    const [policyNumber, setPolicyNumber] = useState('')
+    const [issuanceDate, setIssuanceDate] = useState('')
+    const [maturityDate, setMaturityDate] = useState('')
+    const [premiumFinal, setPremiumFinal] = useState('')
+    const [sumAssuredFinal, setSumAssuredFinal] = useState('')
+    const [policyIssued, setPolicyIssued] = useState('')
+    const handlePremiumFinal = (e) => {
+        e.preventDefault()
+        setPremiumFinal(e.target.value)
+    }
+    const handleSumAssuredFinal = (e) => {
+        e.preventDefault()
+        setSumAssuredFinal(e.target.value)
+    }
+    const handlePolicyIssued = (e) => {
+        e.preventDefault()
+        setPolicyIssued(e.target.value)
+    }
+    const handlePremiumReceived = (e) => {
+        e.preventDefault()
+        setPremiumReceived(e.target.value)
+    }
+    const handlePolicyNumber = (e) => {
+        e.preventDefault()
+        setPolicyNumber(e.target.value) 
+    }
+    const handleIssuanceDate = (e) => {
+        e.preventDefault()
+        setIssuanceDate(e.target.value)
+    }
+    const handleMaturityDate = (e) => {
+         e.preventDefault()
+        setMaturityDate(e.target.value)
+    }
+    const handleSubmitRecalculatePolicy = async (e) => {
+        e.preventDefault()
+        // console.log(customerVerificationDone, active, financialHealthPoints, bankUWResult)
+        const pr = premiumReceived==='Yes'?true:false
+        const pn = Number(policyNumber)
+        const issd = Number(issuanceDate) 
+        const matd = Number(maturityDate)
+        const pf = Number(premiumFinal)
+        const saf = Number(sumAssuredFinal)
+        const pi = policyIssued==='Yes'?true:false
+
+        console.log(pr, pn, issd, matd, pf, saf, pi)
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+        web3.eth.getAccounts().then(console.log);
+        if (web3) {
+            const accounts = await web3.eth.getAccounts()
+            const networkId = await web3.eth.net.getId()
+            const networkData = BlockSecureDeployer.networks[networkId]
+            if (networkData) {
+                const deployedContract = new web3.eth.Contract(BlockSecureDeployer.abi, networkData.address)
+                console.log(deployedContract, 'to change state', accounts[0], insurance.ID, 'also')
+                
+                // confirm details to be called.
+                await deployedContract.methods.RecalculatePolicy(Number(insurance.ID)-1, pr, pn, issd, matd, pf, saf, pi).send({ from: accounts[0] })
+                // update db
+                insurance.state = pi===true?6:-1;
+                insurance.premiumReceived=pr;
+                insurance.policyNumber=pn;
+                insurance.issuanceDate=issd;
+                insurance.maturityDate=matd;
+                insurance.premiumFinal=pf;
+                insurance.sumAssuredFinal=saf;
+                insurance.policyIssued=pi;
+                // fetch the current insurance from the db, and update it
+                await axios.put(`http://localhost:3001/api/insurances/${insurance.ID}`, insurance)
+            }
+        }
+    }
+    return (
+        <div>
+            <form>
+
+            <div className="form-group">
+                <label for="premRec"><bold>Premium Received (Yes/No)</bold></label>
+                <input type="text" className="form-control" id="InputPremRec" value={premiumReceived} placeholder="Yes/No" onChange={handlePremiumReceived}/>
+            </div>
+            <div className="form-group">
+                <label for="policyNum"><bold>Policy Number</bold></label>
+                <input type="text" className="form-control" id="InputPolicyNum" value={policyNumber} placeholder="Enter policy number" onChange={handlePolicyNumber}/>
+            </div>
+            <div className="form-group">
+                <label for="issd"><bold>Issuance Date</bold></label>
+                <input type="text" className="form-control" id="InputISSD" value={issuanceDate} placeholder="Enter Issuance Date" onChange={handleIssuanceDate}/>
+            </div>
+            <div className="form-group">
+                <label for="matd"><bold>Maturity Date</bold></label>
+                <input type="text" className="form-control" id="InputMATD" value={maturityDate} placeholder="Enter Maturity Date" onChange={handleMaturityDate}/>
+            </div>
+            <div className="form-group">
+                <label for="premF"><bold>Final Premium</bold></label>
+                <input type="text" className="form-control" id="InputPremF" value={premiumFinal} placeholder="Enter final premium" onChange={handlePremiumFinal}/>
+            </div>
+           <div className="form-group">
+                <label for="sumAF"><bold>Final Sum Assured</bold></label>
+                <input type="text" className="form-control" id="InputSumAF" value={sumAssuredFinal} placeholder="Enter final sum assured" onChange={handleSumAssuredFinal}/>
+            </div>
+            <div className="form-group">
+                <label for="polIss"><bold>Policy Issued (Yes/No)</bold></label>
+                <input type="text" className="form-control" id="InputPolIss" value={policyIssued} placeholder="Yes/No" onChange={handlePolicyIssued}/>
+            </div>
+            
+    <button type="submit" class="btn btn-primary" onClick={handleSubmitRecalculatePolicy}>Submit</button>
             </form>
         </div>
     )
 }
 const DisplayInsuranceStatus = ({ currentUser, insurance }) => {
     // based on the user type, we provide them with different functionalities
-
+    // const mapStateDescription = {
+    //     0 : "Applied For Insurance",
+    //     1 : "Confirm Details",
+    //     2 : "Send to Bank Underwriter",
+    // }
     if (insurance===null) return (
         <div>Loading Insurances... </div>
     )
     if (currentUser.user.type==='individual') {
+        console.log(insurance, 'now')
         return (
             <div>
             <InsuranceDetails insurance={insurance}/> 
             {insurance.state===0&&<ConfirmDetails insurance={insurance}/>}
-            {insurance.state!==0&&<div>Processing...</div>}
+            {insurance.state===5&&<div>The policy is being recalculated.</div>}
+            {insurance.state===-1&&<div>Your Application was rejected.</div>}
+            {insurance.state===6&&<ConfirmPolicyFinal insurance={insurance}/>}
+            {insurance.state===7&&<div>Your have confirmed the policy. You are eligible for invoking claims </div>}
             </div>
         )
     } else if (currentUser.user.type==='Insurer' && insurance.state>=1) {
         return (
             <div>
                 <InsuranceDetails insurance={insurance}/>
-                {insurance.state==1&&<RequestBankUnderwriting insurance={insurance}/>}
-                {insurance.state==2&&<div>Requested Bank Underwriting</div>}
-                {insurance.state==4&&<div>Requested Medical Underwriting</div>}
+                {insurance.state===1&&<RequestBankUnderwriting insurance={insurance}/>}
+                {insurance.state===2&&<div>Requested Bank Underwriting</div>}
+                {insurance.state===3&&<RequestMedicalUnderwriting insurance={insurance}/>}
+                {insurance.state===4&&<div>Requested Medical Underwriting</div>}
+                {insurance.state===5&&<RecalculatePolicy insurance={insurance}/>}
+                {insurance.state===6&&<div>The insurance application was approved. Waiting for client confirmation.</div>}
+                {insurance.state===7&&<div>The client has accepted the policy </div>}
+                {insurance.state===-1&&<div>The insurance application was rejected</div>}
             </div>
         )
     } else if (currentUser.user.type==='Bank') {
@@ -183,7 +461,13 @@ const DisplayInsuranceStatus = ({ currentUser, insurance }) => {
             <div>
                 Bank func here
                 <InsuranceDetails insurance={insurance}/>
-                {insurance.state==2&&<UpdateFinancialHealth insurance={insurance}/>}
+                {insurance.state===2&&<UpdateFinancialHealth insurance={insurance}/>}
+                {insurance.state===3&&<div>Financial Health Updated. Sent to Insurance Provider. </div>}
+                {insurance.state===4&&<div>Requested Medical Underwriting</div>}
+                {insurance.state===5&&<div>Medical Underwriting Done. Health Score Updated.</div>}
+                {insurance.state===6&&<div>The insurance application was approved. Waiting for client confirmation </div>}
+                {insurance.state===7&&<div>The client has accepted the policy </div>}
+                {insurance.state===-1&&<div>The insurance application was rejected</div>}
             </div>
         )
     } else if (currentUser.user.type==='Hospital') {
@@ -191,6 +475,11 @@ const DisplayInsuranceStatus = ({ currentUser, insurance }) => {
             <div>
                 Hospital func here
                 <InsuranceDetails insurance={insurance}/>
+                {insurance.state===4&&<UpdateMedicalHealth insurance={insurance}/>}
+                {insurance.state===5&&<div>Medical Underwriting Done. Health Score Updated.</div>}
+                {insurance.state===6&&<div>The insurance application was approved. Waiting for client confirmation </div>}
+                {insurance.state===7&&<div>The client has accepted the policy </div>}
+                {insurance.state===-1&&<div>The insurance application was rejected</div>}
             </div>
         )
     }
